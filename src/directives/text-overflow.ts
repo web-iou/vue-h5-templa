66ab13e1f600d643a6796b1c1e6b6c69d01e42cf
cc <cx@ytbig.onaliyun.com>
2023-04-18 17:01:29 +0800

    feat: 完成自定义文本省略指令

--- src/directives/text-overflow.ts
+++ src/directives/text-overflow.ts
@@ -1,14 +1,13 @@
 import type { Directive, App } from "vue";
 const ellipsisDirective: Directive = {
-  beforeMount(el: HTMLElement, binding) {
-    el.style.textOverflow = "ellipsis";
-    el.style.whiteSpace = "nowarp";
-    el.style.overflow = "hidden";
+  mounted(el: HTMLElement, binding) {
+    el.style.cssText =
+      (binding.value
+        ? `-webkit-line-clamp:${binding.value};display: -webkit-box;-webkit-box-orient:vertical;word-wrap: break-word;`
+        : "white-space:nowrap;") + "text-overflow:ellipsis;overflow:hidden;";
   },
 };
-
 export function setupEllipsisDirective(app: App) {
   app.directive("ellipsis", ellipsisDirective);
 }
-
 export default ellipsisDirective;
